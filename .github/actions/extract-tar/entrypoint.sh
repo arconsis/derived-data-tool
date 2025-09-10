c7168bf7884b27a6a7dd9e9f2f80529a6c33a112
Moritz Ellerbrock <github@elmoritz.eu>
2025-09-10 18:37:02 +0200

    add better extract logic

--- .github/actions/extract-tar/entrypoint.sh
+++ .github/actions/extract-tar/entrypoint.sh
@@ -5,4 +5,13 @@ outdir="$2"
 echo "[extract-tar] Extracting $archive to $outdir"
 mkdir -p "$outdir"
 tar -xzf "$archive" -C "$outdir"
+shopt -s nullglob
+files=("$outdir"/*)
+if [ ${#files[@]} -eq 1 ] && [ -d "${files[0]}" ]; then
+	folder="${files[0]}"
+	echo "[extract-tar] Moving files from $folder to $outdir"
+	mv "$folder"/* "$outdir/"
+	rmdir "$folder"
+fi
+shopt -u nullglob
 echo "[extract-tar] Extraction complete."
